6c69ae0f2e6848dc396ef9519a917b41a60afe54
pauldambra <pauldambra@gmail.com>
2016-09-06 21:07:48 +0100

    move rake files to the root of the solution

--- rakefile.rb
+++ rakefile.rb
@@ -3,28 +3,40 @@ require 'albacore/tasks/versionizer'
 
 ::Albacore::Tasks::Versionizer.new :versioning
 
-NUGET_PATH = "./Nuget.exe"
-NUNIT_RUNNER = "../packages/NUnit.Runners.2.6.4/tools/nunit-console.exe"
+NUGET_PATH = "./Build/Nuget.exe"
+NUNIT_RUNNER = "./packages/NUnit.Runners.2.6.4/tools/nunit-console.exe"
 
-MOQ_35_PATTERN =  /\<HintPath\>\.\.\\packages\\Moq\.4\.0\.10827\\lib\\NET35\\Moq\.dll\<\/HintPath\>/
-MOQ_35_HINT = '<HintPath>..\packages\Moq.4.0.10827\lib\NET35\Moq.dll</HintPath>'
-MOQ_40_PATTERN =  /\<HintPath\>\.\.\\packages\\Moq\.4\.0\.10827\\lib\\NET40\\Moq\.dll\<\/HintPath\>/
-MOQ_40_HINT = '<HintPath>..\packages\Moq.4.0.10827\lib\NET40\Moq.dll</HintPath>'
-MODULUS_CHECKING_TESTS_CSPROJ = '../ModulusCheckingTests/ModulusCheckingTests.csproj'
+MOQ_35_PATTERN =  /\<HintPath\>\.\\packages\\Moq\.4\.0\.10827\\lib\\NET35\\Moq\.dll\<\/HintPath\>/
+MOQ_35_HINT = '<HintPath>.\packages\Moq.4.0.10827\lib\NET35\Moq.dll</HintPath>'
+MOQ_40_PATTERN =  /\<HintPath\>\.\\packages\\Moq\.4\.0\.10827\\lib\\NET40\\Moq\.dll\<\/HintPath\>/
+MOQ_40_HINT = '<HintPath>.\packages\Moq.4.0.10827\lib\NET40\Moq.dll</HintPath>'
+MODULUS_CHECKING_TESTS_CSPROJ = './ModulusCheckingTests/ModulusCheckingTests.csproj'
 # task :print_versions => :versioning do
 # 	puts ENV["BUILD_VERSION"]
 # 	puts ENV["NUGET_VERSION"]
 # 	puts ENV["FORMAL_VERSION"]
 # end
 
-desc 'restore all nugets as per the packages.config files'
+task :dot_net_35 => [:build_35, :tests_35]
+task :dot_net_4 => [:build_40, :tests_40]
+task :dot_net_45 => [:build_45, :tests_45]
+task :default => [
+  :versioning,
+  :restore,
+  :clean,
+  :dot_net_35,
+  :dot_net_4,
+  :dot_net_45,
+  :create_nugets
+]
+
 nugets_restore :restore do |p|
   p.out = 'nuget'
   p.exe = NUGET_PATH
 end
 
 build :clean do |b|
-  b.sln = '../ModulusChecking.sln'
+  b.sln = './ModulusChecking.sln'
   b.target = 'Clean'
 end
 
@@ -42,7 +54,7 @@ end
 
 
 build :build_35 => [:use_moq_35] do |b|
-  b.sln = '../ModulusChecking.sln'
+  b.sln = './ModulusChecking.sln'
   b.prop 'TargetFrameworkVersion', 'v3.5'
   b.prop 'outdir', 'bin/Release-netv35/'
   b.logging = 'normal'
@@ -51,14 +63,14 @@ build :build_35 => [:use_moq_35] do |b|
 end
 
 build :build_40 => [:use_moq_40] do |b|
-  b.sln = '../ModulusChecking.sln'
+  b.sln = './ModulusChecking.sln'
   b.prop 'TargetFrameworkVersion', 'v4.0'
   b.prop 'outdir', 'bin/Release-netv4/'
   b.prop 'Configuration', 'Release'
 end
 
 build :build_45 => [:use_moq_40] do |b|
-  b.sln = '../ModulusChecking.sln'
+  b.sln = './ModulusChecking.sln'
   b.prop 'TargetFrameworkVersion', 'v4.5'
   b.prop 'outdir', 'bin/Release-netv45/'
   b.prop 'Configuration', 'Release'
@@ -70,15 +82,15 @@ def run_tests(files)
 end
 
 task :tests_35 do
-  run_tests FileList['../*Tests/bin/Release-netv35/*Tests.dll'].join(' ')
+  run_tests FileList['./*Tests/bin/Release-netv35/*Tests.dll'].join(' ')
 end
 
 task :tests_40 do
-    run_tests FileList['../*Tests/bin/Release-netv4/*Tests.dll'].join(' ')
+    run_tests FileList['./*Tests/bin/Release-netv4/*Tests.dll'].join(' ')
 end
 
 task :tests_45 do
-    run_tests FileList['../*Tests/bin/Release-netv45/*Tests.dll'].join(' ')
+    run_tests FileList['./*Tests/bin/Release-netv45/*Tests.dll'].join(' ')
 end
 
 directory 'nuget'
@@ -87,7 +99,7 @@ desc 'package nugets - finds all projects and package them'
 nugets_pack :create_nugets do |p|
   puts ENV["NUGET_VERSION"]
 
-  p.files   = FileList['../ModulusChecking/ModulusChecking.csproj']
+  p.files   = FileList['./ModulusChecking/ModulusChecking.csproj']
   p.out     = 'nuget'
   p.exe     = NUGET_PATH
   p.with_metadata do |m|
@@ -102,16 +114,3 @@ nugets_pack :create_nugets do |p|
     p.add_file 'bin/Release-netv45/ModulusChecker.dll', 'lib/net45'
   end
 end
-
-task :dot_net_35 => [:build_35, :tests_35]
-task :dot_net_4 => [:build_40, :tests_40]
-task :dot_net_45 => [:build_45, :tests_45]
-task :default => [
-  :versioning,
-  :restore,
-  :clean,
-  :dot_net_35,
-  :dot_net_4,
-  :dot_net_45,
-  :create_nugets
-]
