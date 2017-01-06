dbb89fa2977bc3eb494ed60c18d336cd4529158b
Mohammad Shahab Rafiq <mohammed.shahab@accesspay.com>
2017-01-06 11:29:02 +0000

    Added performance test to support processing of large batches of data.

---
+++ PublicInterfaceTests/PerformanceTests.cs
@@ -0,0 +1,36 @@
+﻿using ModulusChecking;
+using NUnit.Framework;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace PublicInterfaceTests
+{
+    public class PerformanceTests
+    {
+        [TestCase(2)]
+        public void ItCanProcessALargeFileInUnder(int seconds)
+        {
+            var stopwatch = new Stopwatch();
+            var modulusChecker = new ModulusChecker();
+
+            using (var sr = new StreamReader("sa.txt"))
+            {
+                stopwatch.Start();
+
+                while (sr.Peek() >= 0)
+                {
+                    var segments = sr
+                        .ReadLine()
+                        .Split('\t');
+
+                    modulusChecker.CheckBankAccount(segments.First(), segments.Last());
+                }
+
+                stopwatch.Stop();
+            }
+            
+            Assert.IsTrue(stopwatch.Elapsed.Seconds <= seconds, string.Format("Failed to process a large number of sortcodes and account numbers in under {0} seconds.", seconds));
+        }
+    }
+}
