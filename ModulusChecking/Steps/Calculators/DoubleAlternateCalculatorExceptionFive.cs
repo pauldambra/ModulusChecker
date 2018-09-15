c628090392b26dace4687a8824e7646f2b29befa
pauldambra <pauldambra@gmail.com>
2018-09-15 10:59:21 +0100

    throw a specific exception

--- ModulusChecking/Steps/Calculators/DoubleAlternateCalculatorExceptionFive.cs
+++ ModulusChecking/Steps/Calculators/DoubleAlternateCalculatorExceptionFive.cs
@@ -7,12 +7,10 @@ namespace ModulusChecking.Steps.Calculators
 {
     internal class FirstDoubleAlternateCalculatorExceptionFive : DoubleAlternateCalculatorExceptionFive
     {
-        protected override int GetWeightSumForStep(BankAccountDetails bankAccountDetails)
-        {
-            return new DoubleAlternateModulusCheck().GetModulusSum(bankAccountDetails,
-                                                            bankAccountDetails.WeightMappings
-                                                                              .First());
-        }   
+        protected override int GetWeightSumForStep(BankAccountDetails bankAccountDetails) 
+            => new DoubleAlternateModulusCheck().GetModulusSum(
+                bankAccountDetails,
+                bankAccountDetails.WeightMappings.First());
     }
 
     internal class SecondDoubleAlternateCalculatorExceptionFive : DoubleAlternateCalculatorExceptionFive
@@ -21,25 +19,31 @@ namespace ModulusChecking.Steps.Calculators
         {
             if (bankAccountDetails.WeightMappings.Count() != 2)
             {
-                throw new ArgumentOutOfRangeException(
-                    string.Format("This calculator is for step two but the provided details only have {0} mappings",
-                                  bankAccountDetails.WeightMappings.Count()));
+                throw new MustHaveTwoWeightMappings(bankAccountDetails.WeightMappings.Count());
             }
 
-            return new DoubleAlternateModulusCheck().GetModulusSum(bankAccountDetails,
-                                                                   bankAccountDetails.WeightMappings
-                                                                                     .Second());
+            return new DoubleAlternateModulusCheck().GetModulusSum(
+                bankAccountDetails,
+                bankAccountDetails.WeightMappings.Second());
         }
     }
 
-    abstract class DoubleAlternateCalculatorExceptionFive : BaseModulusCalculator
+    internal class MustHaveTwoWeightMappings : Exception
+    {
+        public MustHaveTwoWeightMappings(int mappingsCount) 
+            : base($"This calculator is for step two but the provided details only have {mappingsCount} mappings")
+        {
+        }
+    }
+
+    internal abstract class DoubleAlternateCalculatorExceptionFive : BaseModulusCalculator
     {
         protected abstract int GetWeightSumForStep(BankAccountDetails bankAccountDetails);
 
         /// <summary>
         /// /Perform the second double alternate check, and for the double alternate check with exception 5 the 
         /// checkdigit is h from the original account number, except:
-        /// • After dividing the result by 10;
+        /// ï¿½ After dividing the result by 10;
         /// - if the remainder=0 and h=0 the account number is valid
         /// - for all other remainders, take the remainder away from 10. If the number you get is the same as h 
         /// then the account number is valid.
